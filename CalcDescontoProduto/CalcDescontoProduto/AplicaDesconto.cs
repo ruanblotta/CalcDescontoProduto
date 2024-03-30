3584656da4ac5f7a05ce2a0e2b7f80b82285cc8e
Ruan Blotta <ruanblotta@gmail.com>
2024-03-30 20:58:51 -0300

    Implementando Orientação a Objetos

---
+++ CalcDescontoProduto/CalcDescontoProduto/AplicaDesconto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalcDescontoProduto
+{
+    public class AplicaDesconto
+    {
+        public string NomeProduto;
+        public double ValorProduto;
+        public int QtdProduto;
+
+        public void CalcDesconto()
+        {
+
+            if (QtdProduto <= 10)
+            {
+                double valorTotal = ValorProduto * QtdProduto;
+                Console.WriteLine("O valor a vista da compra é de R$ " + valorTotal);
+            }
+            else if (QtdProduto >= 11 && QtdProduto <= 20)
+            {
+                double desc10 = ValorProduto * QtdProduto * 0.90;
+                Console.WriteLine("O valor total da compra com desconto de 10% é de R$ " + desc10.ToString("F2"));
+            }
+            else if (QtdProduto >= 21 && QtdProduto <= 50)
+            {
+                double desc20 = ValorProduto * QtdProduto * 0.80;
+                Console.WriteLine("O valor total da compra com desconto de 20% é de R$ " + desc20.ToString("F2"));
+            }
+            else if (QtdProduto > 50)
+            {
+                double desc25 = ValorProduto * QtdProduto * 0.75;
+                Console.WriteLine("O valor total da com´ra com desconto de 25% é de R$ " + desc25.ToString("F2"));
+            }
+        }
+    }
+}
--- CalcDescontoProduto/CalcDescontoProduto/Program.cs
+++ CalcDescontoProduto/CalcDescontoProduto/Program.cs
@@ -6,37 +6,20 @@ namespace CalcDescontoProduto
     {
         static void Main(string[] args)
         {
+
+            AplicaDesconto desconto = new AplicaDesconto();
+
             Console.WriteLine("Digite o nome do produto: ");
-            string nomeProduto = Console.ReadLine();
+            desconto.NomeProduto = Console.ReadLine();
 
             Console.WriteLine("Digite o valor do produto: ");
-            double valorProduto = double.Parse(Console.ReadLine());
+            desconto.ValorProduto = double.Parse(Console.ReadLine());
 
             Console.WriteLine("Digite a quantidade que deseja comprar: ");
-            int qtdProduto = int.Parse(Console.ReadLine());
+            desconto.QtdProduto = int.Parse(Console.ReadLine());
 
-
-
-            if (qtdProduto <= 10)
-            {
-                double valorTotal = valorProduto * qtdProduto;
-                Console.WriteLine("O valor a vista da compra é de R$ " + valorTotal);
-            }
-            else if (qtdProduto >= 11 && qtdProduto <=  20)
-            {
-                double desc10 = valorProduto * qtdProduto * 0.90; 
-                Console.WriteLine("O valor total da compra com desconto de 10% é de R$ " + desc10);
-            }
-            else if (qtdProduto >= 21 && qtdProduto <= 50)
-            {
-                double desc20 = valorProduto * qtdProduto * 0.80;
-                Console.WriteLine("O valor total da compra com desconto de 20% é de R$ " + desc20);
-            }
-            else if (qtdProduto > 50)
-            {
-                double desc25 = valorProduto * qtdProduto * 0.75;
-                Console.WriteLine("O valor total da com´ra com desconto de 25% é de R$ " + desc25);
-            }
+            desconto.CalcDesconto();
+          
         }
     }
 }
